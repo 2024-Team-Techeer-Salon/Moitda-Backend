9b36462308177e85fb96a8b87f08701b3329a87b
gang1241 <gang1241@gmail.com>
2024-05-05 03:08:43 +0900

    commit

--- src/main/java/com/techeersalon/moitda/MoitdaApplication.java
+++ src/main/java/com/techeersalon/moitda/MoitdaApplication.java
@@ -2,8 +2,9 @@ package com.techeersalon.moitda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MoitdaApplication {
 
 	public static void main(String[] args) {
