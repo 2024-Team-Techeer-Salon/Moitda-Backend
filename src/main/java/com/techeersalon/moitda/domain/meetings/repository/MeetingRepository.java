25b321cc40e05e71a46e09d843f8d13f5b6f9cae
gang1241 <gang1241@gmail.com>
2024-05-12 13:36:11 +0900

    fix : 미팅 post부분만

--- src/main/java/com/techeersalon/moitda/domain/meetings/repository/MeetingRepository.java
+++ src/main/java/com/techeersalon/moitda/domain/meetings/repository/MeetingRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
     List<Meeting> findByUserId(Long userId);
+
+    Optional<Meeting> findById(Long id);
 }
