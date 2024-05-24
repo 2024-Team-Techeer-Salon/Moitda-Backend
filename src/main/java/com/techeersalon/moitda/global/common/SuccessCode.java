2a0c176c2dad566be0b68ea320db7b908f124cc0
gang1241 <gang1241@gmail.com>
2024-05-24 14:56:04 +0900

    feat : 미팅 타이틀 검색 기능 추가

--- src/main/java/com/techeersalon/moitda/global/common/SuccessCode.java
+++ src/main/java/com/techeersalon/moitda/global/common/SuccessCode.java
@@ -21,18 +21,17 @@ public enum SuccessCode {
     MEETING_PAGING_GET_SUCCESS(HttpStatus.OK, "M004", "모임 페이징 조회 성공"),
     MEETING_UPDATE_SUCCESS(HttpStatus.NO_CONTENT, "M005", "모임 수정 성공"),
     MEETING_END_SUCCESS(HttpStatus.NO_CONTENT, "M006", "모임 종료 성공"),
+    MEETING_SEARCH_SUCCESS(HttpStatus.OK, "M007", "모임 검색 성공"),
 
     //participant
     PARTICIPANT_CREATE_SUCCESS(HttpStatus.CREATED, "P001", "참가자 생성 성공"),
     PARTICIPANT_APPROVAL_OR_REJECTION_SUCCESS(HttpStatus.NO_CONTENT, "P002", "참가자 승인 또는 거절 성공"),
     PARTICIPANT_LIST_GET_SUCCESS(HttpStatus.OK, "P003", "모임 신청자 목록 조회"),
 
-
     //review
     REVIEW_CREATE_SUCCESS(HttpStatus.CREATED, "R001", "후기 생성 성공"),
 
     ;
-
     private HttpStatus status;
     private String code;
     private String message;
