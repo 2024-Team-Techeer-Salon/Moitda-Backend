77dc500ce369a723458e364084e00a9d0110f1a8
yunjuwon <a77265192@gmail.com>
2024-06-03 18:41:49 +0900

    chore : 로컬 환경에서 정상작동

--- src/main/java/com/techeersalon/moitda/MoitdaApplication.java
+++ src/main/java/com/techeersalon/moitda/MoitdaApplication.java
@@ -9,8 +9,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MoitdaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoitdaApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(MoitdaApplication.class, args);}
 
 }
