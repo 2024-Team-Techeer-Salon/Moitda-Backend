0a283c5fc3743fc05969e37136b066ff33d8bf81
yunjuwon <a77265192@gmail.com>
2024-06-10 18:17:26 +0900

    chore: application.yml , 로컬환경에서 log하면서 잘작동

--- src/main/java/com/techeersalon/moitda/MoitdaApplication.java
+++ src/main/java/com/techeersalon/moitda/MoitdaApplication.java
@@ -1,14 +1,26 @@
 package com.techeersalon.moitda;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MoitdaApplication {
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory(".")
+				.load();
+		// 모든 환경 변수를 시스템 속성으로 설정
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
 
-	public static void main(String[] args) {SpringApplication.run(MoitdaApplication.class, args);}
+		// 애플리케이션 종료 시 환경 변수를 삭제하는 후크 추가
+
+
+		SpringApplication.run(MoitdaApplication.class, args);}
 
 }
